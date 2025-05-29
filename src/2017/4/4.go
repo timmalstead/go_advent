facaa107064b3ec38a189d9436998519b47302cb
timmalstead <timmalstead@gmail.com>
2025-05-29 08:44:23 -0700

    improves upon first part of 2017's fourth day

--- src/2017/4/4.go
+++ src/2017/4/4.go
@@ -18,7 +18,6 @@ func main() {
 	var data, err = os.ReadFile("./src/2017/4/input_2.txt")
 
 	if err == nil {
-
 		var text = fmt.Sprintf(ToString, data)
 		var splitOnNewLines = strings.Split(text, NewLines)
 
@@ -27,14 +26,10 @@ func main() {
 			var trimmedLine = strings.Trim(line, DoubleQuotes)
 			var splitOnSpaces = strings.Split(trimmedLine, Space)
 
-			var wordFreqency = make(map[string]int)
+			var wordFreqency, allWordsAreUnique = make(map[string]int), true
 			for _, word := range splitOnSpaces {
 				wordFreqency[word]++
-			}
-
-			var allWordsAreUnique bool = true
-			for _, frequency := range wordFreqency {
-				if frequency > 1 {
+				if wordFreqency[word] > 1 {
 					allWordsAreUnique = false
 					break
 				}

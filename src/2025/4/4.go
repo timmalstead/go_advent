c231d1b266b278b751dbdc7666277bd991b56bd1
timmalstead <timmalstead@gmail.com>
2025-12-04 23:27:08 -0800

    refactors solve for first part of 2025's fourth day

--- src/2025/4/4.go
+++ src/2025/4/4.go
@@ -8,12 +8,13 @@ import (
 )
 
 type Roll struct {
-	x     int
-	y     int
+	x, y  int
 	value string
 }
 
-const CoordTemplate = "x%dy%d"
+func formatCoord(x, y int) string {
+	return fmt.Sprintf("x%dy%d", x, y)
+}
 
 func main() {
 	var coords = make(map[string]Roll)
@@ -25,54 +26,61 @@ func main() {
 	for y, line := range splitInput {
 		var splitLine = strings.Split(line, "")
 		for x, char := range splitLine {
-			coords[fmt.Sprintf(CoordTemplate, x, y)] = Roll{x: x, y: y, value: char}
+			if char == "@" {
+				coords[formatCoord(x, y)] = Roll{x: x, y: y, value: char}
+			}
 		}
 	}
 
 	var mainWaitGroup sync.WaitGroup
 	var readSurroundingCoords = func(key string) {
 		defer mainWaitGroup.Done()
+		var totalNumberOfSurroundingRolls int
 
 		var coord = coords[key]
-		var x, y, currentValue = coord.x, coord.y, coord.value
+		var x, y = coord.x, coord.y
 
-		if currentValue == "@" {
-			var totalNumberOfSurroundingRolls int
+		var coordsToCheck = []string{
+			formatCoord(x+1, y),   // right
+			formatCoord(x+1, y+1), // right down
+			formatCoord(x, y+1),   // down
+			formatCoord(x-1, y+1), // left down
+			formatCoord(x-1, y),   // left
+			formatCoord(x-1, y-1), // left up
+			formatCoord(x, y-1),   // up
+			formatCoord(x+1, y-1), // up right
+		}
 
-			var coordsToCheck = []string{
-				fmt.Sprintf(CoordTemplate, x+1, y),   // right
-				fmt.Sprintf(CoordTemplate, x+1, y+1), // right down
-				fmt.Sprintf(CoordTemplate, x, y+1),   // down
-				fmt.Sprintf(CoordTemplate, x-1, y+1), // left down
-				fmt.Sprintf(CoordTemplate, x-1, y),   // left
-				fmt.Sprintf(CoordTemplate, x-1, y-1), // left up
-				fmt.Sprintf(CoordTemplate, x, y-1),   // up
-				fmt.Sprintf(CoordTemplate, x+1, y-1), // up right
-			}
+		var subWaitGroup sync.WaitGroup
+		var checkSingleCoord = func(coordKey string) {
+			defer subWaitGroup.Done()
 
-			for _, coordKey := range coordsToCheck {
-				var checkingCoord, checkingCoordExists = coords[coordKey]
-
-				if checkingCoordExists && checkingCoord.value == "@" {
-					totalNumberOfSurroundingRolls++
-				}
-			}
-
-			if totalNumberOfSurroundingRolls < 4 {
-				// the mutex is important here as I am accessing and modifying a shared variable
+			var _, checkingCoordExists = coords[coordKey]
+			if checkingCoordExists {
 				mutex.Lock()
-				totalNumberOfAccessibleRolls++
+				totalNumberOfSurroundingRolls++
 				mutex.Unlock()
 			}
+		}
+
+		subWaitGroup.Add(len(coordsToCheck))
+		for _, coordKey := range coordsToCheck {
+			go checkSingleCoord(coordKey)
+		}
+
+		subWaitGroup.Wait()
+		if totalNumberOfSurroundingRolls < 4 {
+			// the mutex is important here as I am accessing and modifying a shared variable
+			mutex.Lock()
+			totalNumberOfAccessibleRolls++
+			mutex.Unlock()
 		}
 	}
 
 	mainWaitGroup.Add(len(coords))
-
 	for key := range coords {
 		go readSurroundingCoords(key)
 	}
-
 	mainWaitGroup.Wait()
 
 	fmt.Println(totalNumberOfAccessibleRolls)

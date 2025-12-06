8bc6b0350f3ba54078ab06159de51b21ffbdcf9e
timmalstead <timmalstead@gmail.com>
2025-12-05 16:43:25 -0800

    refactors solve for first part of 2025's fifth day

--- src/2025/5/5.go
+++ src/2025/5/5.go
@@ -5,6 +5,7 @@ import (
 	"go_advent/src/2025/5/input"
 	"strconv"
 	"strings"
+	"sync"
 )
 
 type Range struct {
@@ -15,30 +16,54 @@ func main() {
 	var splitInput = strings.Split(input.PuzzleInput, "\n\n")
 	var freshIngredientRanges = strings.Split(splitInput[0], "\n")
 
+	var waitGroup sync.WaitGroup
+	var mutex sync.Mutex
+
 	var freshIngredientIdRanges = []Range{}
 
-	for _, ingredientRange := range freshIngredientRanges {
+	var addRange = func(ingredientRange string) {
+		defer waitGroup.Done()
+
 		var splitRange = strings.Split(ingredientRange, "-")
 
 		var lowerBound, _ = strconv.Atoi(splitRange[0])
 		var upperBound, _ = strconv.Atoi(splitRange[1])
 
+		mutex.Lock()
 		freshIngredientIdRanges = append(freshIngredientIdRanges, Range{lowerBound, upperBound})
+		mutex.Unlock()
 	}
 
-	var totalOfFreshIngredients int
+	waitGroup.Add(len(freshIngredientRanges))
+	for _, ingredientRange := range freshIngredientRanges {
+		go addRange(ingredientRange)
+	}
 
+	waitGroup.Wait()
+
+	var totalOfFreshIngredients int
 	var ingredientList = strings.Split(splitInput[1], "\n")
-	for _, ingredientId := range ingredientList {
+
+	var checkRange = func(ingredientId string) {
+		defer waitGroup.Done()
 		var idToCheck, _ = strconv.Atoi(ingredientId)
 
 		for _, freshIngredientRange := range freshIngredientIdRanges {
 			if idToCheck >= freshIngredientRange.lowerBound && idToCheck <= freshIngredientRange.upperBound {
+				mutex.Lock()
 				totalOfFreshIngredients++
+				mutex.Unlock()
 				break
 			}
 		}
 	}
+
+	waitGroup.Add(len(ingredientList))
+	for _, ingredientId := range ingredientList {
+		go checkRange(ingredientId)
+	}
+
+	waitGroup.Wait()
 
 	fmt.Println(totalOfFreshIngredients)
 }
